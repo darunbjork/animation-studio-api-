5e4a239d5ec7cb664736bb34e5a371de36481561
darunbjork <darunbjork@gmail.com>
2026-01-12 14:05:36 +0100

    feat: Add GraphQL type definitions

---
+++ src/infra/graphql/typeDefs.ts
@@ -0,0 +1,36 @@
+import { gql } from "apollo-server-express";
+
+export const typeDefs = gql`
+  type Asset {
+    id: ID!
+    name: String!
+    type: String!
+    currentVersion: Int!
+    versions: [AssetVersion!]!
+    pipeline: AssetPipeline
+    dependencies: [AssetDependency!]!
+  }
+
+  type AssetVersion {
+    version: Int!
+    createdAt: String!
+    changeNote: String
+  }
+
+  type AssetPipeline {
+    status: String!
+    updatedAt: String!
+    error: String
+  }
+
+  type AssetDependency {
+    parentAssetId: ID!
+    childAssetId: ID!
+    type: String!
+  }
+
+  type Query {
+    asset(id: ID!): Asset
+    assets: [Asset!]!
+  }
+`;
