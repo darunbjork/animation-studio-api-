de9a3e30a823b5920e17f544658dd81e47c3b32e
darunbjork <darunbjork@gmail.com>
2026-01-12 17:03:54 +0100

    feat: Integrate correlationId logging in AssetController

--- src/app/controllers/AssetController.ts
+++ src/app/controllers/AssetController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import { AssetService } from "../services/AssetService";
 import { AssetListService } from "../services/AssetListService";
+import { logger } from "../../infra/logging/logger"; // Import logger
 
 export class AssetController {
   static async create(req: Request, res: Response) {
+    // @ts-ignore
+    const correlationId = req.correlationId;
     const asset = await AssetService.createAsset({
       ...req.body,
       // @ts-ignore
@@ -12,10 +15,19 @@ export class AssetController {
       createdBy: req.user.userId,
     });
 
+    logger.info("Asset created", {
+      correlationId,
+      assetId: asset._id,
+      studioId: asset.studioId,
+      createdBy: asset.createdBy,
+    });
+
     res.status(201).json(asset);
   }
 
   static async list(req: Request, res: Response) {
+    // @ts-ignore
+    const correlationId = req.correlationId;
     const page = Number(req.query.page || 1);
     const limit = Number(req.query.limit || 20);
 
@@ -26,20 +38,38 @@ export class AssetController {
       limit
     );
 
+    logger.info("Assets listed", {
+      correlationId,
+      studioId: req.user.studioId,
+      page,
+      limit,
+      count: result.data.length,
+    });
+
     res.json(result);
   }
 
   static async get(req: Request, res: Response) {
+    // @ts-ignore
+    const correlationId = req.correlationId;
     const asset = await AssetService.getAsset(
       req.params.id,
       // @ts-ignore
       req.user.studioId
     );
 
+    logger.info("Asset retrieved", {
+      correlationId,
+      assetId: asset._id,
+      studioId: asset.studioId,
+    });
+
     res.json(asset);
   }
 
   static async update(req: Request, res: Response) {
+    // @ts-ignore
+    const correlationId = req.correlationId;
     const asset = await AssetService.updateAsset(
       req.params.id,
       // @ts-ignore
@@ -47,10 +77,19 @@ export class AssetController {
       req.body
     );
 
+    logger.info("Asset updated", {
+      correlationId,
+      assetId: asset._id,
+      studioId: asset.studioId,
+      changes: req.body,
+    });
+
     res.json(asset);
   }
 
   static async delete(req: Request, res: Response) {
+    // @ts-ignore
+    const correlationId = req.correlationId;
     await AssetService.deleteAsset(
       req.params.id,
       // @ts-ignore
@@ -59,10 +98,18 @@ export class AssetController {
       req.user.role
     );
 
+    logger.info("Asset deleted", {
+      correlationId,
+      assetId: req.params.id,
+      studioId: req.user.studioId,
+    });
+
     res.status(204).send();
   }
 
   static async rollback(req: Request, res: Response) {
+    // @ts-ignore
+    const correlationId = req.correlationId;
     const { version } = req.body;
     await AssetService.rollbackAsset(
       req.params.id,
@@ -70,6 +117,13 @@ export class AssetController {
       req.user.studioId,
       version
     );
+
+    logger.info("Asset rollback initiated", {
+      correlationId,
+      assetId: req.params.id,
+      studioId: req.user.studioId,
+      version,
+    });
 
     res.status(200).json({ message: `Asset rolled back to version ${version}` });
   }
