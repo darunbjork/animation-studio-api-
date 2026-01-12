567993b5cad715d1511b3eba9923c28e77acf036
darunbjork <darunbjork@gmail.com>
2026-01-12 17:38:47 +0100

    fix: Remove @ts-ignore for correlationId in AssetController.ts

--- src/app/controllers/AssetController.ts
+++ src/app/controllers/AssetController.ts
@@ -5,7 +5,6 @@ import { logger } from "../../infra/logging/logger"; // Import logger
 
 export class AssetController {
   static async create(req: Request, res: Response) {
-    // @ts-ignore
     const correlationId = req.correlationId;
     const asset = await AssetService.createAsset({
       ...req.body,
@@ -24,7 +23,6 @@ export class AssetController {
   }
 
   static async list(req: Request, res: Response) {
-    // @ts-ignore
     const correlationId = req.correlationId;
     const page = Number(req.query.page || 1);
     const limit = Number(req.query.limit || 20);
@@ -37,7 +35,7 @@ export class AssetController {
 
     logger.info("Assets listed", {
       correlationId,
-      studioId: req.user.studioId,
+      studioId: req.user!.studioId, // Add non-null assertion
       page,
       limit,
       count: result.data.length,
@@ -47,7 +45,6 @@ export class AssetController {
   }
 
   static async get(req: Request, res: Response) {
-    // @ts-ignore
     const correlationId = req.correlationId;
     const asset = await AssetService.getAsset(
       req.params.id,
@@ -65,7 +62,6 @@ export class AssetController {
   }
 
   static async update(req: Request, res: Response) {
-    // @ts-ignore
     const correlationId = req.correlationId;
     const asset = await AssetService.updateAsset(
       req.params.id,
@@ -85,7 +81,6 @@ export class AssetController {
   }
 
   static async delete(req: Request, res: Response) {
-    // @ts-ignore
     const correlationId = req.correlationId;
     await AssetService.deleteAsset(
       req.params.id,
@@ -97,14 +92,13 @@ export class AssetController {
     logger.info("Asset deleted", {
       correlationId,
       assetId: req.params.id,
-      studioId: req.user.studioId,
+      studioId: req.user!.studioId, // Add non-null assertion
     });
 
     res.status(204).send();
   }
 
   static async rollback(req: Request, res: Response) {
-    // @ts-ignore
     const correlationId = req.correlationId;
     const { version } = req.body;
     await AssetService.rollbackAsset(
@@ -117,7 +111,7 @@ export class AssetController {
     logger.info("Asset rollback initiated", {
       correlationId,
       assetId: req.params.id,
-      studioId: req.user.studioId,
+      studioId: req.user!.studioId, // Add non-null assertion
       version,
     });
 
